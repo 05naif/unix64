e300a797f3c49efd2a549432b4833806d1a2f9cb
05naif <juan.dpmz05@gmail.com>
2025-12-27 17:59:26 -0500

    p: frozen

--- source/p.s
+++ source/p.s
@@ -8,42 +8,57 @@
 # p-command stands for 'print' and it acts like a cat
 # command but w/o all of its capabilities
 #
-# usage: $ p [filename]
+# usage:
+# $ p
+#   or
+# $ p [filename]
 # 
 
 .section .rodata
 	.UsageMessage: .string "usage: p [filename]\n"
-	.UsageLength:  .long 20
+	.UsageLength:  .quad 20
 
-	.ReadingBufferLength: .quad 512
+	.UnexistentFileMessage: .string "error: cannot open file\n"
+	.UnexistentFileLength:  .quad 25
 
 .section .bss
 	.ReadingBuffer: .zero 512
+	.WritenBytes: .zero 2
 
 .section .text
+	.equ ReadingBufferLength, 512
 
 .globl _start
 
 _start:
-	popq	%rax
+	movq	(%rsp), %rax
 	cmpq	$2, %rax
-	jnz		.print_usage
-	popq	%rax
-	popq	%rax
+	jz		.file_provided
+	cmpq	$1, %rax
+	jz		.read_from_stdin
+	jmp		.print_usage
+.read_from_stdin:
+	movq	$0, %r15
+	jmp		.reading_loop
+.file_provided:
+	movq	16(%rsp), %rax
 	movq	%rax, %rdi
 	xorq	%rsi, %rsi
 	movq	$2, %rax
 	syscall
+	cmpq	$0, %rax
+	jl		.unexistent_file
 	movq	%rax, %r15
 .reading_loop:
 	xorq	%rax, %rax
 	movq	%r15, %rdi
 	leaq	.ReadingBuffer(%rip), %rsi
-	movq	.ReadingBufferLength(%rip), %rdx
+	xorq	%rdx, %rdx
+	movw	$ReadingBufferLength, %dx
 	syscall
-	cmpq	$0, %rax
+	cmpw	$0, %ax
 	jz		.stop_reading
-	movq	%rax, %rdx
+	movw	%ax, %dx
 	movq	$1, %rax
 	movq	$1, %rdi
 	leaq	.ReadingBuffer(%rip), %rsi
@@ -60,8 +75,19 @@ _start:
 	leaq	.UsageMessage(%rip), %rsi
 	movq	.UsageLength(%rip), %rdx
 	syscall
-	jmp		_fini
+	jmp		_killit
+.unexistent_file:
+	movq	$1, %rax
+	movq	$2, %rdi
+	leaq	.UnexistentFileMessage(%rip), %rsi
+	movq	.UnexistentFileLength(%rip), %rdx
+	syscall
+	jmp		_killit
 _fini:
 	movq	$60, %rax
 	movq	$0, %rdi
+	syscall
+_killit:
+	movq	$60, %rax
+	movq	$1, %rdi
 	syscall
